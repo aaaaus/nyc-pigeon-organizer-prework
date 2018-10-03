93bf89c5845cad140407220a835312f80edb97b6
Austin Luft <ausluft@gmail.com>
2018-10-03 02:11:10 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do | key1, value1 |
+    value1.each do | key2, value2array |
+      value2array.each do | name |
+        pigeon_list[name] = {}
+      end
+    end
+  end
+  
+  data.each do | key1, value1 |
+    value1.each do | key2, value2array |
+      value2array.each do | name |
+        if name == pigeon_list[name]
+        pigeon_list[name] = key1[value1]
+  
+  pigeon_list.each do | key_name, blank_value |
+    
+  
+  
+  return pigeon_list
 end
